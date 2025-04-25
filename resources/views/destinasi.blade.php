452d1b589809d6053edba7725ab0bfd2403e6713
WahhhDwiPutra <tkebo20@gmail.com>
2025-04-25 20:26:54 +0800

    update route and create view

---
+++ resources/views/destinasi.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Pandawa Trip</title>
+</head>
+
+<body>
+    <h1 class="mt-10 text-3xl font-bold text-center text-red-400">
+        Welcome to Destinasi Pandawa Trip
+    </h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/paket', function () {
+    return view('paket');
+});
+
+Route::get('/destinasi', function () {
+    return view('destinasi');
+});
+Route::get('/about', function () {
+    return view('about');
 });
