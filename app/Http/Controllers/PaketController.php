1f09adb8f667ac2ed7530154e6d513fc0f81b969
WahhhDwiPutra <tkebo20@gmail.com>
2025-04-29 18:36:36 +0800

    update controller lagi

--- app/Http/Controllers/PaketController.php
+++ app/Http/Controllers/PaketController.php
@@ -8,15 +8,21 @@ use Illuminate\Http\Request;
 
 class PaketController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $pakets = PaketTravel::all();
-        return view('pages.paket.index', compact('pakets'));
+        $search = $request->input('search'); // Ambil parameter pencarian dari request
+
+        // Jika ada parameter pencarian, filter data berdasarkan nama atau atribut lain
+        $pakets = PaketTravel::when($search, function ($query, $search) {
+            return $query->where('nama', 'like', '%' . $search . '%');
+        })->get();
+
+        return view('pages.paket.index', compact('pakets', 'search'));
     }
 
     public function show($id)
     {
         $paket = PaketTravel::findOrFail($id);
-        return view('pages.paketDetail.index.', compact('paket'));
+        return view('pages.paketDetail.index', compact('paket'));
     }
 }
