7f17e3ac52758bcc762ddd49795ed7621faa8f76
dharma herz <126298627+dexart15@users.noreply.github.com>
2025-05-02 11:45:09 +0800

    Update team member images in tentangKami page

--- resources/views/pages/tentangKami/teamCards.blade.php
+++ resources/views/pages/tentangKami/teamCards.blade.php
@@ -11,39 +11,40 @@
 
         <div class="card flex flex-col h-96 drop-shadow-lg bg-white p-10 rounded-team text-center justify-center items-center">
             <div class="image-container w-48 h-48 rounded-full overflow-hidden">
-                <img src="image/" alt="Dharma" class="w-full h-full  object-cover">
+                <img src="image/profil-githa.png" alt="Githa" class="w-full h-full object-cover">
             </div>
             <h1 class="text-2xl font-semibold text-secondary mt-5">
-                Dharma Sukawiyana
+                Githa Amyguna
             </h1>
-            <p class="text-lg text-primary">
-                Orang Gaul
+            <p class="text-lg text-primary mt-2">
+                Project Manager & QC
             </p>
         </div>
 
         <div class="card flex flex-col h-96 drop-shadow-lg bg-white p-10 rounded-team text-center justify-center items-center">
             <div class="image-container w-48 h-48 rounded-full overflow-hidden">
-                <img src="image/" alt="Dharma" class="w-full h-full  object-cover">
+                <img src="image/profil-dharma.jpg" alt="Dharma" class="w-full h-full  object-cover">
             </div>
             <h1 class="text-2xl font-semibold text-secondary mt-5">
                 Dharma Sukawiyana
             </h1>
-            <p class="text-lg text-primary">
-                Orang Gaul
+            <p class="text-lg text-primary mt-2">
+                Front-end
             </p>
         </div>
 
         <div class="card flex flex-col h-96 drop-shadow-lg bg-white p-10 rounded-team text-center justify-center items-center">
             <div class="image-container w-48 h-48 rounded-full overflow-hidden">
-                <img src="image/" alt="Dharma" class="w-full h-full  object-cover">
+                <img src="image/profil-wahyu.jpg" alt="Wahyu" class="w-full h-full  object-cover">
             </div>
             <h1 class="text-2xl font-semibold text-secondary mt-5">
-                Dharma Sukawiyana
+                Wahyu Dwi Putra
             </h1>
-            <p class="text-lg text-primary">
-                Orang Gaul
+            <p class="text-lg text-primary mt-2">
+                Back-end
             </p>
         </div>
+
 
     </div>
 
@@ -51,25 +52,25 @@
 
         <div class="card flex flex-col w-4/12 h-96 drop-shadow-lg bg-white p-10 rounded-team text-center justify-center items-center">
             <div class="image-container w-48 h-48 rounded-full overflow-hidden">
-                <img src="image/" alt="Dharma" class="w-full h-full  object-cover">
+                <img src="image/profil-raiko.jpg" alt="Raiko" class="w-full h-full  object-cover">
             </div>
             <h1 class="text-2xl font-semibold text-secondary mt-5">
-                Dharma Sukawiyana
+                Raiko Juspika
             </h1>
-            <p class="text-lg text-primary">
-                Orang Gaul
+            <p class="text-lg text-primary mt-2">
+                UI/UX Designer
             </p>
         </div>
 
         <div class="card flex flex-col w-4/12 h-96 drop-shadow-lg bg-white p-10 rounded-team text-center justify-center items-center">
             <div class="image-container w-48 h-48 rounded-full overflow-hidden">
-                <img src="image/" alt="Dharma" class="w-full h-full  object-cover">
+                <img src="image/profil-gede.jpg" alt="Dharma" class="w-full h-full  object-cover">
             </div>
             <h1 class="text-2xl font-semibold text-secondary mt-5">
-                Dharma Sukawiyana
+                Mahesa Sedana
             </h1>
-            <p class="text-lg text-primary">
-                Orang Gaul
+            <p class="text-lg text-primary mt-2">
+                UI/UX Designer
             </p>
         </div>
     </div>
