1315b66f0bd10644ee4a692e74b532f796db7cc1
dharma herz <126298627+dexart15@users.noreply.github.com>
2025-06-16 23:36:24 +0800

    update favicon link path for correct resource loading

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="image/logo-pandawa-trip-invert.svg">
+    <link rel="icon" href="/image/logo-pandawa-trip-invert.svg">
     <link rel="stylesheet" href="{{ asset('css/splide.min.css') }}">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
